a09242688f91ec99515b19ab385528eaabce2b7a
AlexanderAliaga19 <u202417693@upc.edu.pe>
2026-05-12 01:49:39 -0500

    feat: I raised the environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+/**
+ * @author: Alexander Auden Aliaga Ocampo
+ * codigo:U202417693
+ */
+
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://frontpulse-api.onrender.com',
+  appName: 'Care-Labs / PulseReport',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+/**
+ * @author: Alexander Auden Aliaga Ocampo
+ * codigo:U202417693
+ */
+
+/**
+ * Environment configuration for development.
+ * Care-Labs / PulseReport
+ */
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:3000',
+  appName: 'Care-Labs / PulseReport',
+};
