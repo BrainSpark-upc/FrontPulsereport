841c86900315a08f8ed4bb49d070d29261ef1733
Johancuba <johan.giovani18@gmail.com>
2026-05-12 04:35:55 -0500

    feat(patient)
    define patient response DTO

---
+++ src/app/patient/infrastructure/patient-response.ts
@@ -0,0 +1,13 @@
+export interface PatientResponse {
+  id: string;
+  code: string;
+  firstName: string;
+  lastName: string;
+  documentNumber: string;
+  dateOfBirth: string;
+  gender: string;
+  phone: string;
+  email: string;
+  status: string;
+  roomNumber: string;
+}
